14c5fdf427d9033abbc579b0ad2230272368861c
jonathanlukass <jhonatanlukas05@gmail.com>
2024-05-09 23:22:01 +0800

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import SplashScreen from './SplashScreen';
 import SignWith from './SignWith';
 import loginAdmin from './loginAdmin';
-import loginStudents from './loginStudents'
+import loginStudents from './loginStudents';
+import Profile from './Profile';
+import Statistics from './Statistics';
+import Home from './Menu';
 
-export {SplashScreen, SignWith, loginAdmin, loginStudents};
+export {
+  SplashScreen,
+  SignWith,
+  loginAdmin,
+  loginStudents,
+  Profile,
+  Statistics,
+  Home,
+};
--- src/pages/loginStudents/index.tsx
+++ src/pages/loginStudents/index.tsx
@@ -1,59 +1,56 @@
-import {StyleSheet, View,Text, ImageBackground} from 'react-native';
+import {StyleSheet, View, Text, ImageBackground} from 'react-native';
 import React from 'react';
-import {Button, Gap,TextInput} from '../../components';
-import { backGround } from '../../assets/images';
-
+import {Button, Gap, TextInput} from '../../components';
+import {backGround} from '../../assets/images';
 
 const loginStudents = ({navigation}) => {
-    return (
-        <ImageBackground source={backGround} style={styles.backgroundImage}>      
-        <Text style={{ 
-         fontSize: 25, 
-         textAlign: 'center', 
-         fontFamily: 'Poppins-Black', 
-         fontWeight: '', 
-         color: 'white', 
-         marginTop: 30  }}>
-           Student
-         </Text>             
-           <View style={styles.contentWrapper}>
-           <Gap height={30} />
-           <Text style={{ 
-             fontSize: 30, 
-             textAlign: 'center', 
-             fontFamily: 'Poppins-Black', 
-             fontWeight: 'bold', 
-             color: '#333', 
-             marginTop: 5  }}>
-               FILKOM UNKLAB
-           </Text>
-           <Gap height={5} />
-             <TextInput
-               label=""
-               placeholder="  email"
-             />
-             <Gap height={1} />
-             <TextInput label="" 
-               placeholder="  password" 
-             />
-               <Gap height={35} />
-             <Button
-               label="Log In"
-               backgroundColor="#8D92A3"
-               textColor="#FFFFFF"
-               onPress={() => navigation.navigate('SplashScreen')}
-             />
-               <Gap height={12} />
-               <Button
-               label="Back"
-               backgroundColor="#8D92A3"
-               textColor="#FFFFFF"
-               onPress={() => navigation.navigate('SignWith')}
-               />
-        
-        </View>
-     </ImageBackground>
-    );
+  return (
+    <ImageBackground source={backGround} style={styles.backgroundImage}>
+      <Text
+        style={{
+          fontSize: 25,
+          textAlign: 'center',
+          fontFamily: 'Poppins-Black',
+          fontWeight: '',
+          color: 'white',
+          marginTop: 30,
+        }}>
+        Student
+      </Text>
+      <View style={styles.contentWrapper}>
+        <Gap height={30} />
+        <Text
+          style={{
+            fontSize: 30,
+            textAlign: 'center',
+            fontFamily: 'Poppins-Black',
+            fontWeight: 'bold',
+            color: '#333',
+            marginTop: 5,
+          }}>
+          FILKOM UNKLAB
+        </Text>
+        <Gap height={5} />
+        <TextInput label="" placeholder="  email" />
+        <Gap height={1} />
+        <TextInput label="" placeholder="  password" />
+        <Gap height={35} />
+        <Button
+          label="Log In"
+          backgroundColor="#8D92A3"
+          textColor="#FFFFFF"
+          onPress={() => navigation.navigate('Home')}
+        />
+        <Gap height={12} />
+        <Button
+          label="Back"
+          backgroundColor="#8D92A3"
+          textColor="#FFFFFF"
+          onPress={() => navigation.navigate('SignWith')}
+        />
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default loginStudents;
@@ -61,7 +58,6 @@ export default loginStudents;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
   contentWrapper: {
     marginTop: 250,
@@ -69,9 +65,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     borderRadius: 30,
     width: 350,
-    height: 400,  
+    height: 400,
     backgroundColor: 'white',
-
   },
   backgroundImage: {
     flex: 1,
